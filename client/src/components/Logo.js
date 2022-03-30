5295f0299701b173abe42bdc177f9a9683845efc
hosik-GitHub <fuctrock@gmail.com>
2022-03-30 21:50:23 +0900

    Taskcard:메인바 구현2(프론트엔드) insert Navbar css

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import logo from '../images/logo.png';
 import { Link } from 'react-router-dom';
-import style from './Logo.css';
 
 const Logo = () => {
     return(
-        <header className={style.header}>
-        <div className={style.logo}>
             <Link to="/">
-                <img src={logo} alt="logo image" className={style.logo} />
+                <img src={logo} alt="logo image" />
             </Link>
-        </div>
-        </header>
+
     )
 
     }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import style from './Navbar.css';
 import Logo from './Logo';
 import { Link } from 'react-router-dom';
+import "../App.css"
 
 
 
 
 const Navbar = () => {
 return (
-    <header>
-        
-       
-        
-        <Logo />
+  <div className="navbar">
+    <span className="logo"><Logo /></span>
+    <span className="home_button"><nav><a href="/">HOME</a></nav></span>
+    <span>
         <nav>
-          <a href="/">HOME</a>
-          <a href="/signup">Singup</a>
-          <a href="/login">Login</a>
-       <div id="indicator"></div>
-</nav>
-        
-    </header>
+            <a href="/search">Search</a>
+            <a href="/signup">Singup</a>
+            <a href="/login">Login</a>
+        </nav>
+    </span>
+    {/* <div id="indicator"></div> */}
+  </div>
 )
 };
 

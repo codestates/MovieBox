0493aa72b76ffec77354aeb34f19d32e5d6c8571
seonghyeon-Chi <jiji6027@gmail.com>
2022-03-30 22:37:37 +0900

    TaskCard:회원가입1(프론트엔드) making signup page html, css

--- client/src/pages/Singup.js
+++ client/src/pages/Singup.js
@@ -1,2 +1,29 @@
-const Signup = () => <div>Signup</div>;
+import '../App.css'
+
+const Signup = () => 
+    <div className='signup'>
+      <body>
+        <h2>Sign Up</h2>
+        <div>
+          <input type="text" placeholder="email"></input>
+        </div>
+        <div>
+          <input type="text" placeholder="name"></input>
+        </div>
+        <div>
+          <input type="text" placeholder="password"></input>
+        </div>
+        <div>
+          <input type="text" placeholder="conform password"></input>
+        </div>
+        <div>
+          <input type="text" placeholder="nickname"></input>
+        </div>
+        <div>
+          <button>signup</button>
+        </div>
+        <div>이미 아이디가 있으신가요? <a href="/login">login</a></div>
+      </body>
+    </div>
+
 export default Signup;
